d3b885ef64020256968320ce36a6fe03f5ba34ca
srinathk10 <68668616+srinathk10@users.noreply.github.com>
2024-01-08 22:08:32 -0800

    Update dbt_dag.py

--- airflow/airflow/dags/dbt_dag.py
+++ airflow/airflow/dags/dbt_dag.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta  # Ensure timedelta is imported
 with DAG(
         dag_id="dbt_dag",
         start_date=datetime(2024, 1, 1),  # Adjust start date as needed
-        schedule_interval=timedelta(seconds=60),  # Schedule every 60 seconds
+        schedule_interval=timedelta(seconds=15),  # Schedule every 15 seconds
         catchup=False,
         ) as dag:
     dbt_run = BashOperator(
